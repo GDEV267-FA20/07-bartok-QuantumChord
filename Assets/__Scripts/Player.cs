02222697abb97861236e81c09edce3f5a8740c87
QuantumChord <60354100+QuantumChord@users.noreply.github.com>
2020-11-05 15:39:49 -0500

    Fanning Hands

--- Assets/__Scripts/Player.cs
+++ Assets/__Scripts/Player.cs
@@ -32,6 +32,8 @@ public class Player
         //Add the card to the hand
         hand.Add(eCB);
 
+        FanHand();
+
         return(eCB);
     }
 
@@ -44,6 +46,58 @@ public class Player
 
         hand.Remove(cb);
 
+        FanHand();
+
         return(cb);
+    }
+
+    public void FanHand()
+    {
+        //startRot is the rotation about z of the first card
+        float startRot = 0;
+
+        startRot = handSlotDef.rot;
+
+        if (hand.Count > 1)
+        {
+            startRot += Bartok.S.handFanDegrees * (hand.Count - 1) / 2;
+        }
+
+        //Move all the cards to their new positions
+
+        Vector3 pos;
+
+        float rot;
+
+        Quaternion rotQ;
+
+        for (int i = 0; i<hand.Count; i++)
+        {
+            rot = startRot - Bartok.S.handFanDegrees * i;
+
+            rotQ = Quaternion.Euler(0, 0, rot);
+
+            pos = Vector3.up * CardBartok.CARD_HEIGHT / 2f;
+
+            //Add the base position of the player's hand (which will be at the
+            //bottom-center of the fan of the cards)
+
+            pos += handSlotDef.pos;
+
+            pos.z = -0.5f * i;
+
+            //Set the localPosition and rotation of the ith card in the hand
+            hand[i].transform.localPosition = pos;
+
+            hand[i].transform.rotation = rotQ;
+
+            hand[i].state = CBState.hand;
+
+            hand[i].faceUp = (type == PlayerType.human);
+
+            //Set the SortOrder of the cards so that they overlap properly
+            hand[i].SetSortOrder(i * 4);
+
+        }
     }
 }
